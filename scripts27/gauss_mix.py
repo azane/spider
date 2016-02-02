7fbf7727f60c2a70be75e9da74edb572868c72df
azane <azane@nnu.edu>
2016-02-02 11:00:38 +0900

    adds first pass at tensorboard visualization for debugging

--- scripts27/gauss_mix.py
+++ scripts27/gauss_mix.py
@@ -6,7 +6,6 @@ def shape_range_for_elementwise(r, rank=1):
     #r  the range matrix
     #    [ [rb, rt]
     #      [rb, rt] ]
-    #s sample size of ins or outs pairs
     r = tf.transpose(r) #group rbot and rtop
     rb, rt = tf.split(0, 2, r) #split rbot and rtop
     
@@ -27,147 +26,158 @@ def bias_variable(shape):
     return tf.Variable(initial)
 
 
-#sess = tf.InteractiveSession()
-
+#-----<Mixture Construction>-----
 #environmental inputs
-envSize = 3
+envSize = 4
 #control feature inputs
-conSize = 1
-
-inCount = envSize + conSize
-
-fullIn = tf.placeholder("float", shape=[None, inCount]) #None allows a variable amount of rows, samples in this case
-
-
-#gaussianComponents
-g = 5
+conSize = 0
 #targetDims
 t = 1
+#gaussianComponents
+g = 5
+#-----</Mixture Construction>-----
 
-outCount = g + t + (g*t)
 
-fullOut_ = tf.placeholder("float", shape=[None, t])
+#-----<Placeholder Construction>-----
+inCount = envSize + conSize #for ANN and, by proxy, gaussian mixture
+outCount = g + t + (g*t) #for ANN
+
+fullIn = tf.placeholder("float", shape=[None, inCount], name='x_val') #None allows a variable amount of rows, samples in this case
+
+fullOut_ = tf.placeholder("float", shape=[None, t], name='t_val') #for gaussian mixture
+
 
 #data ranges for elementwise conversion
 #[ [rb, rt]
 #  [rb, rt] ]
-inRange = tf.placeholder("float", shape=[inCount,2]) #2 columns, rtop, rbot
-outRange = tf.placeholder("float", shape=[t,2])
-
-#two hidden layers
-hSize = 5
-
-w1 = weight_variable([inCount, hSize])
-b1 = bias_variable([hSize])
-
-w2 = weight_variable([hSize, hSize])
-b2 = bias_variable([hSize])
-
-w3 = weight_variable([hSize, outCount])
-b3 = bias_variable([outCount])
-
-def _old():
-	#get output constants distribution, but mix and make constant
-	#FIXME this was to prevent too much weight sharing...but i can figure outhow to [: [1,4,5]] index in tensorflow! so drop it for contiguousness.
-	#i = np.arange(0, outCount)
-	#np.random.shuffle(i) #in place
-
-	#get indices for gauss constants
-	#mixIndex = tf.constant(i[0:g])
-	#np.expand_dims(mixIndex, axis=0) #add a dimension for gather to broadcast over samples.
-	#varIndex = tf.constant(i[g:g+t])
-	#np.expand_dims(varIndex, axis=0)
-	#meanIndex = tf.constant(i[g+t:outCount])
-	#meanIndex.reshape((g,t)) ultimately, this needs to be in this shape.
-	#np.expand_dims(meanIndex, axis=0)
-	pass
-
-#sess.run(tf.initialize_all_variables())
+inRange = tf.placeholder("float", shape=[inCount,2], name="inRange") #2 columns, rtop, rbot
+outRange = tf.placeholder("float", shape=[t,2], name="outRange")
 
 # get and convert range matrices for massaging
 eRInB, eRInT = shape_range_for_elementwise(inRange, rank=2)
 eROutB, eROutT= shape_range_for_elementwise(outRange, rank=3)
-
-# massage inputs to tanh range [-1,1]
-fullIn = (2*(fullIn-eRInB)/(eRInT-eRInB)) - 1
-
-#run ANN
-lay1 = tf.tanh( tf.matmul(fullIn, w1) + b1 )
-lay2 = tf.tanh( tf.matmul(lay1, w2) + b2 )
-netOut = tf.tanh( tf.matmul(lay2, w3) + b3 )
+    
+#summary ops
+x_hist = tf.histogram_summary("x", fullIn)
+t_hist = tf.histogram_summary("t", fullOut_)
+#-----</Placeholder Construction>-----
 
 
-def _old():
-	#TEMP the numpy version of the operations i want are below. but i can't find a good way to do it in tensorflow.
-	#netOut[:, mixIndex]
-	#varRaw[:, varIndex]
-	#meanRaw[:, meanIndex].reshape((g,t))
+#-----<ANN Construction>-----
+with tf.name_scope('ANN_const') as scope:
+    #two hidden layers of size:
+    hSize = 5
 
-	#split up fullOut into constants for gaussian mixture, gather over rows
-	#mixRaw = tf.gather(netOut, mixIndex)
-	#varRaw = tf.gather(netOut, varIndex)
-	#meanRaw = tf.gather(netOut, meanIndex)
-	pass
+    w1 = weight_variable([inCount, hSize])
+    b1 = bias_variable([hSize])
 
-#parse and shape netOut into coefficients
-mixRaw = tf.slice(netOut, begin=[0,0], size=[-1,g])
-varRaw = tf.slice(netOut, begin=[0,g], size=[-1,t])
-meanRaw = tf.reshape(tf.slice(netOut, begin=[0,g+t], size=[-1,-1]), shape=[-1,g,t])
+    w2 = weight_variable([hSize, hSize])
+    b2 = bias_variable([hSize])
+
+    w3 = weight_variable([hSize, outCount])
+    b3 = bias_variable([outCount])
+
+#summary ops
+w1_hist = tf.histogram_summary("w1", w1)
+b1_hist = tf.histogram_summary("b1", b1)
+
+w2_hist = tf.histogram_summary("w2", w2)
+b2_hist = tf.histogram_summary("b2", b2)
+
+w3_hist = tf.histogram_summary("w3", w3)
+b3_hist = tf.histogram_summary("b3", b3)
+#-----</ANN Construction>-----
+
+#-----<ANN Execution>-----
+with tf.name_scope('ANN_exec') as scope:
+    # massage inputs to tanh range [-1,1]
+    netIn = (2*(fullIn-eRInB)/(eRInT-eRInB)) - 1
+
+    #run ANN
+    lay1 = tf.tanh( tf.matmul(netIn, w1) + b1 )
+    lay2 = tf.tanh( tf.matmul(lay1, w2) + b2 )
+    netOut = tf.tanh( tf.matmul(lay2, w3) + b3 )
+
+    #parse and shape netOut activations into coefficient arrays.
+    mixRaw = tf.slice(netOut, begin=[0,0], size=[-1,g]) #.shape == [s,g]
+    varRaw = tf.slice(netOut, begin=[0,g], size=[-1,t]) #.shape == [s,t]
+    meanRaw = tf.reshape(tf.slice(netOut, begin=[0,g+t], size=[-1,-1]), shape=[-1,g,t]) #.shape == [s,g,t]
+#-----</ANN Execution>-----
 
 
+#-----<Massage for Gaussian Mixture>-----
 #massaging functions, prep ANN outputs for use in gaussian mixture, see notes for explanation
 
-#mixing coefficient
-mixExp = tf.exp(mixRaw) #e^ each element of mixRaw
-mixSum = tf.reduce_sum(mixExp, 1, keep_dims=True) #reduce each sample to scalar, total of all, keep dims for broadcasting over samples
-mix = tf.div(mixExp, mixSum) #divide each mixing coefficient by the total.
+with tf.name_scope('massage_mix') as scope:
+    #mixing coefficient
+    mixExp = tf.exp(mixRaw) #e^ each element of mixRaw
+    mixSum = tf.reduce_sum(mixExp, 1, keep_dims=True) #reduce each sample to scalar, total of all, keep dims for broadcasting over samples
+    mix = tf.div(mixExp, mixSum) #divide each mixing coefficient by the total.
 
-#variances
-var = tf.exp(tf.mul(varRaw, 2)) #keep variance positive, and relevant. this scales from tanh output (-1,1)
+with tf.name_scope('massage_var') as scope:
+    #variances
+    var = tf.exp(tf.mul(varRaw, 2)) #keep variance positive, and relevant. this scales from tanh output (-1,1)
 
-#mean
-#expand to output range
-mean = ((.5 + (meanRaw/2)) * (eROutT - eROutB)) + eROutB #this scales from relevant tanh output (-1,1)
+with tf.name_scope('massage_mean') as scope:
+    #mean
+    #expand to output range
+    mean = ((.5 + (meanRaw/2)) * (eROutT - eROutB)) + eROutB #this scales from relevant tanh output (-1,1)
+
+#summary ops
+mix_hist = tf.histogram_summary("Mixing Coefficients", mix)
+var_hist = tf.histogram_summary("Variances", var)
+mean_hist = tf.histogram_summary("Means", mean)
+#-----</Massage for Gaussian Mixture>-----
 
 
 def mixture_negative_log_likelihood(m, v, u, t, sess=None):
-    #(s is the sample size, g is the numbe of gaussian components, t is the number of target components)
-    #where m is the mixing coefficent array of shape [s,g]
-    #   v is the variance array of shape [s,t]
-    #   u is the mean (mew) array of shape [s,g,t]
-    #   t is the target array of shape [s,t]
+    with tf.name_scope('mixture_nll') as scope:
+        #(s is the sample size, g is the numbe of gaussian components, t is the number of target components)
+        #where:
+        #   m is the mixing coefficent array of shape [s,g]
+        #   v is the variance array of shape [s,t]
+        #   u is the mean (mew) array of shape [s,g,t]
+        #   t is the target array of shape [s,t]
 
-    #prep terms with variance inside.
-    #add a dimension of 1 to be broadcast over the 'g' dimension in other tensors.
-    v = tf.expand_dims(v, 1) #add after dim index 1 for shape [s, 1, t]
+        #prep terms with variance inside.
+        #add a dimension of 1 to be broadcast over the 'g' dimension in other tensors.
+        v = tf.expand_dims(v, 1) #add after dim index 1 for shape [s, 1, t]
 
-    v_norm = 1/(v*tf.sqrt(2*np.pi))
-    v_dem = 2*tf.square(v)
+        v_norm = 1/(v*tf.sqrt(2*np.pi))
+        v_dem = 2*tf.square(v)
 
-    #prep numerator term with corresponding sample target values and net proposed means
-    t = tf.expand_dims(t, 1) #add dim at 2nd position for broadcasting over means.
-    tm_num = tf.square(t-u)
+        #prep numerator term with corresponding sample target values and net proposed means
+        t = tf.expand_dims(t, 1) #add dim at 2nd position for broadcasting over means.
+        tm_num = tf.square(t-u)
 
-    #employ terms in pre-mixed likelihood function.
-    premix = v_norm*(tf.exp(-(tm_num/v_dem)))
-    if sess: print "premix: " + str(sess.run(premix))
+        #employ terms in pre-mixed likelihood function.
+        premix = v_norm*(tf.exp(-(tm_num/v_dem)))
+        if sess: print "premix: " + str(sess.run(premix))
 
-    #add dim for broadcasting mixing coefficients over t, the 3rd dimension.
-    m = tf.expand_dims(m, 2)
+        #add dim for broadcasting mixing coefficients over t, the 3rd dimension.
+        m = tf.expand_dims(m, 2)
 
-    #mix gaussian components
-    likelihood = m*premix
-    if sess: print "likelihood: " + str(sess.run(likelihood))
+        #mix gaussian components
+        likelihood = m*premix
+        if sess: print "likelihood: " + str(sess.run(likelihood))
 
-    #sum over the likilihood of each target and gaussian component, don't reduce over samples yet.
-    #FIXME i know the gaussian components are supposed to be summed, but i'm not sure about the various targets?
-    #FIXME      do i have to mix the targets like the gaussian components?
-    tot_likelihood = tf.reduce_sum(likelihood, [1,2])
-    if sess: print "tot_likelihood: " + str(sess.run(tot_likelihood))
+        #sum over the likilihood of each target and gaussian component, don't reduce over samples yet.
+        #FIXME i know the gaussian components are supposed to be summed, but i'm not sure about the various targets?
+        #FIXME      do i have to mix the targets like the gaussian components?
+        tot_likelihood = tf.reduce_sum(likelihood, [1,2])
+        if sess: print "tot_likelihood: " + str(sess.run(tot_likelihood))
 
-    #take natural log of sum, then reduce over samples, then negate for the final negative log likelihood
-    nll = -tf.reduce_sum(tf.log(tot_likelihood)) #this reduces along the final dimension, so nll will be a scalar.
-
+        #take natural log of sum, then reduce over samples, then negate for the final negative log likelihood
+        nll = -tf.reduce_sum(tf.log(tot_likelihood)) #this reduces along the final dimension, so nll will be a scalar.
+    
+    #summary ops
+    v_norm_hist = tf.histogram_summary("Variance Normalizer", v_norm)
+    v_dem_hist = tf.histogram_summary("Variance Denominator", v_dem)
+    tm_num_hist = tf.histogram_summary("Target-Mean Numerator", tm_num)
+    premix_hist = tf.histogram_summary("Pre-mixed Likelihood", premix)
+    likelihood_hist = tf.histogram_summary("Post-mixed Likelihood", likelihood)
+    nll_hist = tf.scalar_summary("Negative Log Likelihood", nll)
+    
     return nll
 
 
@@ -195,45 +205,50 @@ f_outRange = np.array(
                         ]
                     )
 
-loss = mixture_negative_log_likelihood(mix, var, mean, fullOut_)
+with tf.name_scope('loss') as scope:
+    loss = mixture_negative_log_likelihood(mix, var, mean, fullOut_)
 
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+with tf.name_scope('train_step') as scope:
+    train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
+
+#init session
 sess = tf.Session()
+
+#init summary writer
+merged = tf.merge_all_summaries()
+writer = tf.train.SummaryWriter("data/gauss_mix_logs", sess.graph_def)
+
+#init vars
 sess.run(tf.initialize_all_variables())
 
-for i in range(1):    
-    s_i = np.random.random_integers(0, high=f_fullIn.shape[0]-1, size=300)
-    t_dict = {
-                fullIn:f_fullIn[s_i],
-                inRange:f_inRange,
-                outRange:f_outRange,
-                fullOut_:f_fullOut_[s_i]
-            }
-    
-    sess.run(train_step, feed_dict=t_dict)
-    #print "netout sample: " + str(sess.run([netOut], feed_dict=t_dict)[0])
 
-test = sess.run(loss, feed_dict={
-                                    fullIn:t_fullIn,
-                                    inRange:f_inRange,
-                                    outRange:f_outRange,
-                                    fullOut_:t_fullOut_
-                                })
-print 'final log likilihood of test sample: ' + str(test)
-#[mix, var, mean, netOut]
-#print "actual (mix, var, mean):"
-#print 'mix shape: ' + str(finalout[0].shape)
-#print finalout[0]
-#print 'var shape: ' + str(finalout[1].shape)
-#print finalout[1]
-#print 'mean shape: ' + str(finalout[2].shape)
-#print finalout[2]
+#training loop
+for i in range(1000):
 
+    if i % 10 == 0:
+        s_i = np.random.random_integers(0, high=t_fullIn.shape[0]-1, size=500)
+        f_dict = {
+                    fullIn:t_fullIn[s_i],
+                    inRange:f_inRange,
+                    outRange:f_outRange,
+                    fullOut_:t_fullOut_[s_i]
+                }
 
+        result = sess.run([merged, loss], feed_dict=f_dict)
+        summary_str = result[0]
+        test_loss = result[1]
+        writer.add_summary(summary_str, i)
+        writer.flush()
+        print("Loss at step %s: %s" % (i, test_loss))
 
-#print "netOut:"
-#print finalout[3]
-#print "netOut shape: " + str(finalout[3].shape)
-#print
+    else:
+        s_i = np.random.random_integers(0, high=f_fullIn.shape[0]-1, size=500)
+        f_dict = {
+                    fullIn:f_fullIn[s_i],
+                    inRange:f_inRange,
+                    outRange:f_outRange,
+                    fullOut_:f_fullOut_[s_i]
+                }
 
+        sess.run(train_step, feed_dict=f_dict)
